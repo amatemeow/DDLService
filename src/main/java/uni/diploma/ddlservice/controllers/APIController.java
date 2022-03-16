c64279a84f4edd7e4ca9bb2279e48b3d6f5e180f
amaterasuzxc <igarjar@gmail.com>
2022-03-16 15:51:14 +0300

    Updated GET(bu user) and POST(new schema) requests

--- src/main/java/uni/diploma/ddlservice/controllers/APIController.java
+++ src/main/java/uni/diploma/ddlservice/controllers/APIController.java
@@ -1,28 +1,26 @@
 package uni.diploma.ddlservice.controllers;
 
 import org.springframework.web.bind.annotation.*;
-import uni.diploma.ddlservice.entities.SQLColumn;
-import uni.diploma.ddlservice.entities.SQLConstraint;
-import uni.diploma.ddlservice.entities.SQLSchema;
-import uni.diploma.ddlservice.entities.SQLTable;
+import uni.diploma.ddlservice.entities.*;
 import uni.diploma.ddlservice.enums.SQLColTypes;
 import uni.diploma.ddlservice.enums.SQLConTypes;
 import uni.diploma.ddlservice.exceptions.DDLServiceMissingUserException;
+import uni.diploma.ddlservice.processing.DDLBuilder;
+import uni.diploma.ddlservice.processing.JSONDeserializer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import java.awt.image.AreaAveragingScaleFilter;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @RestController
 public class APIController {
 
-    private static final SQLSchema sampleSchema = new SQLSchema(1L, "exampleUser", "Sample_Schema",
+    private static final SQLSchema sampleSchema = new SQLSchema("exampleUser", "Sample_Schema",
             new ArrayList<>(List.of(new SQLTable("MY_FIRST_TABLE",
             new ArrayList<>(List.of(new SQLColumn("ID", SQLColTypes.NUMBER))),
                     new ArrayList<>(List.of(
-                            new SQLConstraint("MY_ID_UNQ", SQLConTypes.UNIQUE, "ID"),
-                            new SQLConstraint("MY_ID_NOTNULL", SQLConTypes.NOTNULL, "ID")))))));
+                            new SQLConstraint(Optional.of("MY_ID_UNQ"), SQLConTypes.UNIQUE, "ID"),
+                            new SQLConstraint(Optional.of("MY_ID_NOTNULL"), SQLConTypes.NOTNULL, "ID")))))));
     private static final ArrayList<SQLSchema> schemas = new ArrayList<>(List.of(sampleSchema));
 
     @GetMapping("/api/hello")
@@ -30,7 +28,7 @@ public class APIController {
         return "Hello!";
     }
 
-    @GetMapping("/api/schema/{user}")
+    @GetMapping("/api/schema/user/{user}")
     public SQLSchema getUserSchema(@PathVariable String user)
             throws DDLServiceMissingUserException {
         try {
@@ -42,11 +40,11 @@ public class APIController {
 
     @GetMapping("/api/ddl-from-schema/{id}")
     public String getDDL(@PathVariable Long id) {
-        return "HUY";
+        return new DDLBuilder(sampleSchema).build();
     }
 
     @PostMapping("/api/schema/new")
     public String addSchema(@RequestBody Map<String, Object> requestBody) {
-        return "newHUY";
+        return new DDLBuilder(new JSONDeserializer(requestBody).deserialize()).build();
     }
 }
