b9722cfb2bc2a2df494e3c4c51a28d1b1190b617
amaterasuzxc <igarjar@gmail.com>
2022-03-16 10:44:00 +0300

    Initial commit

---
+++ src/main/java/uni/diploma/ddlservice/DdlServiceApplication.java
@@ -0,0 +1,13 @@
+package uni.diploma.ddlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdlServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uni/diploma/ddlservice/DdlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.diploma.ddlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
