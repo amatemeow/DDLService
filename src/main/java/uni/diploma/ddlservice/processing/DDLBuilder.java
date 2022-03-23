06a5bea894f619f6cddfaf8dec8aade65094e735
amaterasuzxc <igarjar@gmail.com>
2022-03-23 17:24:36 +0300

    Few new features 1) Added schema creation if DBadmin 2) Added NOT NULL to CHECK mapping 3) Some improvements to FOREIGN KEY clauses

--- src/main/java/uni/diploma/ddlservice/processing/DDLBuilder.java
+++ src/main/java/uni/diploma/ddlservice/processing/DDLBuilder.java
@@ -1,6 +1,7 @@
 package uni.diploma.ddlservice.processing;
 
 import uni.diploma.ddlservice.entities.*;
+import uni.diploma.ddlservice.enums.SQLConTypes;
 
 import java.util.Optional;
 
@@ -30,25 +31,40 @@ public class DDLBuilder {
             cased = true;
         }
 
+        /*Если указан пользователь БД (схема), и будет стоять галочка создания от лица администратора,
+        * тогда будет сгенерирован код на создание схемы этого пользователя и выделение ему основных привелегий
+        * для работы с базой*/
+        if (sourceSchema.isByRoot() && sourceSchema.getSqlSchema().isPresent()) {
+            DDL.append(String.format("\n\n/*Creating a specified schema %1$s with password %1$s*/",
+                    sourceSchema.getSqlSchema().get()));
+            DDL.append(String.format("\nCREATE USER %1$s IDENTIFIED BY %1$s;", sourceSchema.getSqlSchema().get()));
+            DDL.append(String.format("\nGRANT CREATE SESSION TO %s;", sourceSchema.getSqlSchema().get()));
+            DDL.append(String.format("\nGRANT CONNECT TO %s;", sourceSchema.getSqlSchema().get()));
+        }
+
         for (SQLTable table : sourceSchema.getTables()) {
             String createCurrent = "\n\n/*Creating table " + table.getTableName() + "*/" +
                     "\nCREATE TABLE " + (cased ? "" : "\"") + table.getTableName() + (cased ? "" : "\"") + " \n(";
 
             for (SQLColumn column : table.getColumns()) {
-                createCurrent += "\"" + column.getName() + "\" " + column.getType() + ", \n";
+                createCurrent += "\"" + column.getName() + "\" " + column.getType().toString() + ", \n";
             }
             createCurrent = createCurrent.substring(0, createCurrent.lastIndexOf(", \n"));
 
+            /*NOT NULL ограничение будет закодировано как CHECK ограничение с условием IS NOT NULL*/
             for (SQLConstraint constraint : table.getConstraints()) {
                 createCurrent += ", \n" + "CONSTRAINT " + (cased && constraint.getName().isPresent() ? "" : "\"") +
                         constraint.getName().orElse("") +
                         (cased && constraint.getName().isPresent() ? "" : "\"") + " " +
-                        constraint.getType().toString() + " (\"" + constraint.getColumn() + "\")";
+                        (SQLConTypes.NOTNULL == constraint.getType()
+                                ? SQLConTypes.CHECK.toString() + " (\"" + constraint.getColumn() + "\" IS NOT NULL)"
+                                : constraint.getType().toString() + " (\"" + constraint.getColumn() + "\")");
 
                 if (constraint.getReference().isPresent()) {
                     ForeignReference reference = constraint.getReference().get();
-                    createCurrent += " REFERENCES \"" + reference.getReferenceTable() +
-                            "\" (\"" + reference.getReferenceColumn() + "\")";
+                    createCurrent += " REFERENCES \"" + (sourceSchema.getSqlSchema().isPresent()
+                                    ? sourceSchema.getSqlSchema().get() + "\".\"" : "") +
+                            reference.getReferenceTable() + "\" (\"" + reference.getReferenceColumn() + "\")";
                 }
             }
             createCurrent += ");";
