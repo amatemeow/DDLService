733369705d9dd277adf5bc51f5a6195d598de145
amaterasuzxc <igarjar@gmail.com>
2022-03-23 17:21:36 +0300

    Stocked Column Types ENUM

--- src/main/java/uni/diploma/ddlservice/enums/SQLColTypes.java
+++ src/main/java/uni/diploma/ddlservice/enums/SQLColTypes.java
@@ -1,7 +1,24 @@
 package uni.diploma.ddlservice.enums;
 
 public enum SQLColTypes {
-    NUMBER,
-    VARCHAR2,
-    DATE
+    NUMBER("NUMBER"),
+    FLOAT("FLOAT"),
+    CHAR("CHAR"),
+    VARCHAR2("VARCHAR2(4000)"),
+    CLOB("CLOB"),
+    DATE("DATE"),
+    TIMESTAMP("TIMESTAMP"),
+    RAW("RAW(2000)"),
+    BLOB("BLOB");
+
+    private String representation;
+
+    SQLColTypes(String representation) {
+        this.representation = representation;
+    }
+
+    @Override
+    public String toString() {
+        return representation;
+    }
 }
