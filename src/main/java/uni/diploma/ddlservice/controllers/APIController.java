45884a1d4c52c73250cd33de2c809c3c75ed9cfc
amaterasuzxc <igarjar@gmail.com>
2022-03-28 16:36:48 +0300

    Few changes to APIController 1) Deprecated code deleted 2) Implemented get schema GET 3) Optimized new schema POST 4) Optimized view session GET 5) Added drop session POST 6) Optimized new session POST

--- src/main/java/uni/diploma/ddlservice/controllers/APIController.java
+++ src/main/java/uni/diploma/ddlservice/controllers/APIController.java
@@ -2,71 +2,45 @@ package uni.diploma.ddlservice.controllers;
 
 import org.springframework.web.bind.annotation.*;
 import uni.diploma.ddlservice.entities.*;
-import uni.diploma.ddlservice.enums.SQLColTypes;
-import uni.diploma.ddlservice.enums.SQLConTypes;
-import uni.diploma.ddlservice.exceptions.DDLServiceMissingUserException;
 import uni.diploma.ddlservice.processing.DDLBuilder;
 import uni.diploma.ddlservice.processing.JSONDeserializer;
 
 import javax.servlet.http.HttpSession;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.stream.Collectors;
 
 @RestController
 public class APIController {
 
     private static final ConcurrentLinkedQueue<Session> webSessions = new ConcurrentLinkedQueue<>();
 
-//    private static final SQLSchema sampleSchema = new SQLSchema("exampleUser", "Sample_Schema",
-//            new ArrayList<>(List.of(new SQLTable("MY_FIRST_TABLE",
-//            new ArrayList<>(List.of(new SQLColumn("ID", SQLColTypes.NUMBER))),
-//                    new ArrayList<>(List.of(
-//                            new SQLConstraint(Optional.of("MY_ID_UNQ"), SQLConTypes.UNIQUE, "ID"),
-//                            new SQLConstraint(Optional.of("MY_ID_NOTNULL"), SQLConTypes.NOTNULL, "ID")))))),
-//            true);
-//    private static final ArrayList<SQLSchema> schemas = new ArrayList<>(List.of(sampleSchema));
-
-    @GetMapping("/api/newSession")
+    @PostMapping("/api/newSession")
     public Session initiateNewSession(HttpSession session) {
-        Session response = new Session(session.getId());
-        webSessions.add(response);
+        Session response = webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
+                .findFirst().orElse(new Session(session.getId()));
+        if (!webSessions.contains(response)) {
+            webSessions.add(response);
+        }
         return response;
     }
 
-    //Needs to be implemented
     @GetMapping("/api/schema")
-    public SQLSchema getUserSchema(@RequestParam(required = false) String user)
-            throws DDLServiceMissingUserException {
-//        try {
-//            response = schemas.stream().filter(sch -> sch.getUser().equals(user))
-//                    .collect(Collectors.toList());
-//        } catch (RuntimeException re) {
-//            throw new DDLServiceMissingUserException();
-//        } finally {
-//            if (response.isEmpty()) {
-//                throw new DDLServiceMissingUserException();
-//            }
-//        }
-
-        return new SQLSchema("");
+    public SQLSchema getUserSchema(HttpSession session) {
+        return webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
+                .findFirst().orElse(initiateNewSession(session)).getSessionSchema();
     }
 
     //Needs to be implemented
     @GetMapping("/api/ddl-from-schema")
-    public String getDDL(@PathVariable Long id) {
+    public String getDDL() {
         return new DDLBuilder(new SQLSchema("")).build();
     }
 
     @PostMapping("/api/schema/new")
     public String addSchema(@RequestBody Map<String, Object> requestBody, HttpSession session) {
         SQLSchema schema = new JSONDeserializer(requestBody).deserialize();
-        //May be optimized with inline check
-        if (webSessions.stream().anyMatch(s -> s.getSessionID().equals(session.getId())))
-        {
-            webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
-                    .findFirst().get().associateSchema(schema);
-        }
+        webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
+                .findFirst().orElse(initiateNewSession(session)).associateSchema(schema);
 
         return new DDLBuilder(schema).build();
     }
@@ -74,6 +48,12 @@ public class APIController {
     @GetMapping("/api/viewSession")
     public Session getSession(HttpSession session) {
         return webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
-                .findFirst().orElse(null);
+                .findFirst().orElse(initiateNewSession(session));
+    }
+
+    @PostMapping("/api/dropSession")
+    public void dropSession(HttpSession session) {
+        webSessions.remove(webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
+                .findFirst().orElse(initiateNewSession(session)));
     }
 }
