c5bcbd2c146d80c39623dc34124b94956d317858
amaterasuzxc <igarjar@gmail.com>
2022-03-28 17:51:31 +0300

    Few changes to APIController 1) Implemented get DDL GET 2) Added download GET

--- src/main/java/uni/diploma/ddlservice/controllers/APIController.java
+++ src/main/java/uni/diploma/ddlservice/controllers/APIController.java
@@ -3,9 +3,13 @@ package uni.diploma.ddlservice.controllers;
 import org.springframework.web.bind.annotation.*;
 import uni.diploma.ddlservice.entities.*;
 import uni.diploma.ddlservice.processing.DDLBuilder;
+import uni.diploma.ddlservice.processing.FileBuilder;
 import uni.diploma.ddlservice.processing.JSONDeserializer;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -30,10 +34,15 @@ public class APIController {
                 .findFirst().orElse(initiateNewSession(session)).getSessionSchema();
     }
 
-    //Needs to be implemented
-    @GetMapping("/api/ddl-from-schema")
-    public String getDDL() {
-        return new DDLBuilder(new SQLSchema("")).build();
+    //Optimize with Custom Exception
+    @GetMapping("/api/getDDL")
+    public String getDDL(HttpSession session) throws RuntimeException{
+        try {
+            return new DDLBuilder(webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
+                    .findFirst().get().getSessionSchema()).build();
+        } catch (RuntimeException re) {
+            throw new RuntimeException("Error: trying to build an empty model.");
+        }
     }
 
     @PostMapping("/api/schema/new")
@@ -55,5 +64,20 @@ public class APIController {
     public void dropSession(HttpSession session) {
         webSessions.remove(webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
                 .findFirst().orElse(initiateNewSession(session)));
+    }
+
+    //Optimize Exception
+    //Fix empty file
+    //Need to add auto file-deletion
+    @GetMapping("/api/download")
+    public void getScript(HttpServletResponse response, HttpSession session) throws IOException {
+        try {
+            response.setContentType("application/sql");
+            response.setHeader("Content-Disposition","attachment; filename\"scriptDDL.sql\"");
+            Files.copy(new FileBuilder(getDDL(session), session.getId()).getFile().toPath(), response.getOutputStream());
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
