de4ccd860d6b8ca78a4ce6ed109193225af601db
amaterasuzxc <igarjar@gmail.com>
2022-04-11 20:35:11 +0300

    A lot of useful stuff implemented

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -7,19 +7,53 @@ function newBlock(element) {
     div.innerHTML = sample.innerHTML;
 }
 
-function process() {
-    let schema = document.getElementById('new_schema');
+function removeBlock(element) {
+    element.parentNode.parentNode.remove();
+}
+
+function checkFields() {
     let filled = true;
 
-    schema.querySelectorAll('[required]').forEach(i => {
-       if (!filled) return;
-       if (!i.value) filled = false;
+    document.getElementById('new_schema').querySelectorAll('[required]').forEach(i => {
+        if (!filled) return;
+        if ($(i).is(':visible') && !i.value) filled = false;
     });
 
     if (!filled) {
         alert('Please, fill all required fields!');
-        return;
     }
+    return filled;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function raiseAlert(id, duration) {
+    let alertBox = document.getElementById(id);
+    alertBox.style.display = 'block';
+    window.setTimeout(function(){
+        alertBox.style.opacity = '1';
+        alertBox.style.transform = 'scale(1)';
+    },400);
+    await sleep(duration);
+    alertBox.style.opacity = '0';
+    alertBox.style.transform = 'scale(0)';
+    window.setTimeout(function(){
+        alertBox.style.display = 'none';
+    },400);
+}
+
+function toggleReference(element) {
+    if ($(element).val() === 'FOREIGN_KEY') {
+        $(element.parentNode.parentNode).find('.new-reference-block').toggle(true);
+    } else {
+        $(element.parentNode.parentNode).find('.new-reference-block').toggle(false);
+    }
+}
+
+async function process() {
+    let schema = document.getElementById('new_schema');
 
     let tables = document.getElementsByClassName('new_table_block');
 
@@ -34,59 +68,60 @@ function process() {
         let columns = document.getElementsByClassName('new-col-block');
         let rawcols = [];
         [].forEach.call(columns, c => {
-            rawcols.push({'name': $(c).find('[name="columnName"]').val(),
-                'type': $(c).find('[name="columnType"]').val()});
+            rawcols.push({
+                'name': $(c).find('[name="columnName"]').val(),
+                'type': $(c).find('[name="columnType"]').val()
+            });
         });
         let constraints = document.getElementsByClassName('new-con-block');
         let rawcons = [];
         [].forEach.call(constraints, c => {
-            rawcons.push({'name': $(c).find('[name="constraintName"]').val(),
+            rawcons.push({
+                'name': $(c).find('[name="constraintName"]').val(),
                 'type': $(c).find('[name="constraintType"]').val(),
                 'column': $(c).find('[name="constraintColumn"]').val(),
-                'reference': {
+                'reference': !$(c).find('.new-reference-block').is(':visible') ? null :
+                    {
                     'referenceColumn': $(c).find('[name="referenceColumn"]').val(),
-                    'referenceTable': $(c).find('[name="referenceTable"]').val()}});
+                    'referenceTable': $(c).find('[name="referenceTable"]').val()
+                }
+            });
         });
-        rawtables.push({'tableName': $(t).find('[name="tableName"]').val(),
-            'columns': rawcols, 'constraints': rawcons})
+        rawtables.push({
+            'tableName': $(t).find('[name="tableName"]').val(),
+            'columns': rawcols, 'constraints': rawcons
+        })
     });
 
     raw.tables = rawtables;
     let json = JSON.stringify(raw);
+    console.log(raw);
 
-    // let xhr = new XMLHttpRequest();
-    // xhr.open('POST', '/api/json/download');
-    // xhr.setRequestHeader("Content-type", "application/json")
-    // //set the reponse type to blob since that's what we're expecting back
-    // xhr.responseType = 'blob';
-    // xhr.onload = e => {
-    //     if (this.status == 200) {
-    //         // Create a new Blob object using the response data of the onload object
-    //         var blob = new Blob([this.response], {type: 'application/sql'});
-    //         //Create a link element, hide it, direct it towards the blob, and then 'click' it programatically
-    //         let a = document.createElement("a");
-    //         a.style = "display: none";
-    //         document.body.appendChild(a);
-    //         //Create a DOMString representing the blob and point the link element towards it
-    //         let url = window.URL.createObjectURL(blob);
-    //         a.href = url;
-    //         a.download = 'ddlScript.sql';
-    //         //programatically click the link to trigger the download
-    //         a.click();
-    //         //release the reference to the file by revoking the Object URL
-    //         window.URL.revokeObjectURL(url);
-    //     }else{
-    //         //deal with your error state here
-    //     }
-    // };
-    // xhr.send(json);
-        console.log(raw);
+    const response = await fetch('/api/schema/new', {
+        method: 'POST',
+        body: json,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 
-        const response = fetch('/api/json/download', {
-            method: 'POST',
-            body: json, // string or object
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(resp => {return resp.blob()}).then(blob => {download(blob, 'scriptDDL.sql')});
+    raiseAlert('alert-sv', 2000);
+}
+
+async function downloadScript() {
+    if (!checkFields()) return;
+    await process();
+
+    const response = fetch('/api/download', {
+        method: 'GET'
+    }).then(resp => {
+        return resp.blob()
+    }).then(blob => {
+        download(blob, 'scriptDDL.sql')
+    });
+}
+
+async function resetSession() {
+    const response = await fetch('/api/dropSession', {method: 'POST'});
+    window.location.reload();
 }
