003cea8a7fce63c87e9f42e664ecfb65352179fe
amaterasuzxc <igarjar@gmail.com>
2022-04-11 20:33:58 +0300

    Rebased sessions collection

--- src/main/java/uni/diploma/ddlservice/DdlServiceApplication.java
+++ src/main/java/uni/diploma/ddlservice/DdlServiceApplication.java
@@ -3,10 +3,15 @@ package uni.diploma.ddlservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import uni.diploma.ddlservice.entities.Session;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @ServletComponentScan
 @SpringBootApplication
 public class DdlServiceApplication {
+
+    public static final ConcurrentLinkedQueue<Session> webSessions = new ConcurrentLinkedQueue<>();
 
     public static void main(String[] args) {
         SpringApplication.run(DdlServiceApplication.class, args);
