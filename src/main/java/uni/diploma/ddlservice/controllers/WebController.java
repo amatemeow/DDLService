bfd07d379e7ec3796ae7ddfd8d6399bab1a6ad09
amaterasuzxc <igarjar@gmail.com>
2022-04-11 20:35:46 +0300

    Added reset feature plus improved new Schema view

--- src/main/java/uni/diploma/ddlservice/controllers/WebController.java
+++ src/main/java/uni/diploma/ddlservice/controllers/WebController.java
@@ -4,16 +4,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import uni.diploma.ddlservice.entities.SQLSchema;
+import uni.diploma.ddlservice.entities.Session;
 import uni.diploma.ddlservice.enums.SQLColTypes;
 import uni.diploma.ddlservice.enums.SQLConTypes;
+
+import javax.servlet.http.HttpSession;
+
+import static uni.diploma.ddlservice.DdlServiceApplication.webSessions;
+
 
 @RestController
 public class WebController {
 
     @GetMapping("/new")
-    public ModelAndView newSchema(Model model) {
+    public ModelAndView newSchema(Model model, HttpSession session) {
+        Session localSession = webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
+                .findFirst().orElse(null);
+        SQLSchema schema = localSession == null ? new SQLSchema() : localSession.getSessionSchema();
         model.addAttribute("coltypes", SQLColTypes.values());
         model.addAttribute("contypes", SQLConTypes.values());
+        model.addAttribute("schema", schema);
         return new ModelAndView("new");
+    }
+
+    @GetMapping("/drop")
+    public RedirectView drop(Model model, HttpSession session) {
+        APIController.dropSession(session);
+        newSchema(model, session);
+        return new RedirectView("/new");
     }
 }
