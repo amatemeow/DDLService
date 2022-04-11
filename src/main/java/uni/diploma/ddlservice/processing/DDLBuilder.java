0de6a699344fa420ccda387a2b2c3a85fbae93ff
amaterasuzxc <igarjar@gmail.com>
2022-04-11 20:33:19 +0300

    Quotes on names fix

--- src/main/java/uni/diploma/ddlservice/processing/DDLBuilder.java
+++ src/main/java/uni/diploma/ddlservice/processing/DDLBuilder.java
@@ -3,8 +3,7 @@ package uni.diploma.ddlservice.processing;
 import uni.diploma.ddlservice.entities.*;
 import uni.diploma.ddlservice.enums.SQLConTypes;
 
-import java.util.Optional;
-
+//НУЖНО ДОБАВИТЬ ОБРАБОТКУ CHECK!!!
 public class DDLBuilder {
     private final StringBuilder DDL = new StringBuilder();
     private final SQLSchema sourceSchema; //If initial object gone missing while executing
@@ -14,22 +13,7 @@ public class DDLBuilder {
     }
 
     public String build() {
-        boolean cased = false;
-        if (sourceSchema.getSqlSchema().isPresent()) {
-            String sqlSchemaName = sourceSchema.getSqlSchema().get();
-
-            for (SQLTable table : sourceSchema.getTables()) {
-                table.setTableName("\"" + sqlSchemaName + "\".\"" + table.getTableName() + "\"");
-
-                for (SQLConstraint constraint : table.getConstraints()) {
-                    if (constraint.getName().isPresent()) {
-                        constraint.setName(Optional.of("\"" + sqlSchemaName + "\".\"" +
-                                constraint.getName().get() + "\""));
-                    }
-                }
-            }
-            cased = true;
-        }
+        boolean cased = sourceSchema.getSqlSchema().isPresent();
 
         /*Если указан пользователь БД (схема), и будет стоять галочка создания от лица администратора,
         * тогда будет сгенерирован код на создание схемы этого пользователя и выделение ему основных привелегий
@@ -43,8 +27,10 @@ public class DDLBuilder {
         }
 
         for (SQLTable table : sourceSchema.getTables()) {
-            String createCurrent = "\n\n/*Creating table " + table.getTableName() + "*/" +
-                    "\nCREATE TABLE " + (cased ? "" : "\"") + table.getTableName() + (cased ? "" : "\"") + " \n(";
+            String createCurrent = "\n\n/*Creating table " + (cased ? "\"" + sourceSchema.getSqlSchema().get() + "\"." : "\"") +
+                    "\"" + table.getTableName() + "\"" + "*/" +
+                    "\nCREATE TABLE " + (cased ? "\"" + sourceSchema.getSqlSchema().get() + "\"." : "\"") +
+                    "\"" + table.getTableName() + "\"" + " \n(";
 
             for (SQLColumn column : table.getColumns()) {
                 createCurrent += "\"" + column.getName() + "\" " + column.getType().toString() + ", \n";
