34879c21a405efffddaae515fdfa155970a9e74b
amaterasuzxc <igarjar@gmail.com>
2022-05-12 18:00:23 +0300

    Download function fix + build exception fix

--- src/main/java/uni/diploma/ddlservice/controllers/APIController.java
+++ src/main/java/uni/diploma/ddlservice/controllers/APIController.java
@@ -1,13 +1,16 @@
 package uni.diploma.ddlservice.controllers;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.web.bind.annotation.*;
 import uni.diploma.ddlservice.entities.*;
+import uni.diploma.ddlservice.exceptions.DDLServiceBadRequestException;
 import uni.diploma.ddlservice.processing.DDLBuilder;
 import uni.diploma.ddlservice.processing.FileBuilder;
 import uni.diploma.ddlservice.processing.JSONDeserializer;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.*;
 import java.nio.file.Files;
 import java.util.*;
 
@@ -34,12 +37,12 @@ public class APIController {
 
     //Optimize with Custom Exception
     @GetMapping("/api/getDDL")
-    public String getDDL(HttpSession session) throws RuntimeException{
+    public String getDDL(HttpSession session) {
         try {
             return new DDLBuilder(webSessions.stream().filter(s -> s.getSessionID().equals(session.getId()))
                     .findFirst().get().getSessionSchema()).build();
         } catch (RuntimeException re) {
-            throw new RuntimeException("Error: trying to build an empty model.");
+            throw new DDLServiceBadRequestException("Trying to build inconsistent model!");
         }
     }
 
@@ -66,16 +69,21 @@ public class APIController {
     //Optimize Exception
     //Fix empty file
     @GetMapping("/api/download")
-    public void getScript(HttpServletResponse response, HttpSession session) {
+    public void getScript(HttpServletResponse response, HttpSession session) throws IOException {
         try {
+            FileBuilder fb = new FileBuilder(getDDL(session), session.getId());
             response.setContentType("application/sql");
             response.setHeader("Content-Disposition","attachment; filename\"scriptDDL.sql\"");
-            FileBuilder fb = new FileBuilder(getDDL(session), session.getId());
+            response.setStatus(HttpServletResponse.SC_OK);
             Files.copy(fb.getFile().toPath(), response.getOutputStream());
-            response.getOutputStream().flush();
             fb.deleteFile();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(Arrays.toString(e.getStackTrace()));
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            InputStream error = new ByteArrayInputStream(e.getMessage().getBytes());
+            IOUtils.copy(error, response.getOutputStream());
+        } finally {
+            response.getOutputStream().flush();
         }
     }
 }
